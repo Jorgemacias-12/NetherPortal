262162e46473f5c76bca1c16f1c221fa72ba77f2
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-14 14:00:30 -0500

    Completando función para el calculo.

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function getNewCoords(){
+    var divER = document.getElementById("er");
+    var ocoordX = document.getElementById("coord-x").value;
+    var ocoordz = document.getElementById("coord-z").value;
+    if(ocoordX == "")
+    {
+
+    }
+    else if(ocoordZ == "")
+    {
+
+    }
+    else if(ocoordX == "" && ocoordZ == "")
+    {
+
+    }
+    else
+    {
+
+    }
+}
