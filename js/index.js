5bcb760be3f4208511ad1bec2d8f0d8cb47f30bb
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-14 14:14:42 -0500

    El div de errores aparece en su lugar.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 function getNewCoords(){
-    var divER = document.getElementById("er");
+    var divER = document.getElementById("reContainer");
     var ocoordX = document.getElementById("coord-x").value;
     var ocoordz = document.getElementById("coord-z").value;
     if(ocoordX == "")
     {
-
+        divER.classList.add("errors"); 
+        divER.style.display="block";
     }
     else if(ocoordZ == "")
     {
