c68c6d93dcaa3541db71f28d504a3ebdcd74d892
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-17 15:04:01 -0500

    Validación y lógica terminada.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,45 @@
 function getNewCoords(){
-    var errDiv = document.getElementById("eContainer");
-    var resultDiv = document.getElementById("rContainer");
-    var ocoordX = document.getElementById("coord-x").value;
-    var ocoordZ = document.getElementById("coord-z").value;
+    var inputCoordX = document.getElementById("coord-x").value;
+    var inputCoordZ = document.getElementById("coord-z").value;
+
+    // Validations section
+
+    if(inputCoordX == "")
+    {
+        document.getElementById("eContainer").style.display="flex";
+        document.getElementById("errorMessage").innerHTML="Error: campo x vacío.";
+    }
+    else if (inputCoordZ == "")
+    {
+        document.getElementById("eContainer").style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Error: campo z vacío.";
+    }
+    else
+    {
+        if (inputCoordX == "" && inputCoordZ == "") {
+            document.getElementById("eContainer").style.display = "flex";
+            document.getElementById("errorMessage").innerHTML = "Error: Los campos estan vacios.";
+        }
+    }
+    
+    if(isNaN(inputCoordX) && !isNaN(inputCoordZ))
+    {
+        document.getElementById("eContainer").style.display="flex";
+        document.getElementById("errorMessage").innerHTML="Error: el campo x no contiene números."
+    }
+    else if (!isNaN(inputCoordX) && isNaN(inputCoordZ))
+    {
+        document.getElementById("eContainer").style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Error: el campo z no contiene números."
+    }
+    else if (isNaN(inputCoordX) && isNaN(inputCoordZ))
+    {
+        document.getElementById("eContainer").style.display="flex";
+        document.getElementById("errorMessage").innerHTML="Error: Ninguno de los campos contiene números.";
+    }
+
+    document.getElementById("rContainer").style.display="flex";
+    document.getElementById("resultMessage").innerHTML="X: "+(inputCoordX / 8)+"  Z: "+(inputCoordZ / 8);
 
 }
 
