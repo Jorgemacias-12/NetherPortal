69197123141063e5b0320a1851aa67f9f0af4dd4
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-17 14:25:53 -0500

    Validación de los campos reiniciada para mejor orden.

--- js/index.js
+++ js/index.js
@@ -3,40 +3,7 @@ function getNewCoords(){
     var resultDiv = document.getElementById("rContainer");
     var ocoordX = document.getElementById("coord-x").value;
     var ocoordZ = document.getElementById("coord-z").value;
-    if(ocoordX == "")
-    {
-        errDiv.style.display="flex";
-        document.getElementById("errorMessage").innerHTML="El campo de la coordenada x se encuentra vacío";
-    }
-    else if(ocoordZ == "")
-    {
-        errDiv.style.display = "flex";
-        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
-    }
-    else if(ocoordX == "" && ocoordZ == "")
-    {
-        errDiv.style.display = "flex";
-        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
-    }
-    else if(isNaN(ocoordX))
-    {
-        errDiv.style.display = "flex";
-        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
-    }
-    else if(isNaN(ocoordZ))
-    {
-        errDiv.style.display = "flex";
-        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
-    }
-    else if(isNaN(ocoordX) && isNaN(ocoordZ))
-    {
-        errDiv.style.display = "flex";
-        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
-    }
-    else
-    {
 
-    }
 }
 
 function hideError()
