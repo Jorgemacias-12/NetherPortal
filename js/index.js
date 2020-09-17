8cc9fbe78cde3d24ee352de45e3e863a404d58be
Jorgemacias-12 <jmacias3118@jal.conalep.edu.mx>
2020-09-17 14:12:07 -0500

    Color: #444 añadido al elemento fa-times

--- js/index.js
+++ js/index.js
@@ -5,29 +5,33 @@ function getNewCoords(){
     var ocoordZ = document.getElementById("coord-z").value;
     if(ocoordX == "")
     {
-        errDiv.classList.remove("none");
-        errDiv.classList.add("errors");
-        document.getElementById("errorMessage").innerHTML="Campo coordenada x vacío";
+        errDiv.style.display="flex";
+        document.getElementById("errorMessage").innerHTML="El campo de la coordenada x se encuentra vacío";
     }
     else if(ocoordZ == "")
     {
-
+        errDiv.style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
     }
     else if(ocoordX == "" && ocoordZ == "")
     {
-
+        errDiv.style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
     }
     else if(isNaN(ocoordX))
     {
-
+        errDiv.style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
     }
     else if(isNaN(ocoordZ))
     {
-
+        errDiv.style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
     }
     else if(isNaN(ocoordX) && isNaN(ocoordZ))
     {
-
+        errDiv.style.display = "flex";
+        document.getElementById("errorMessage").innerHTML = "Campo coordenada x vacío";
     }
     else
     {
@@ -37,5 +41,10 @@ function getNewCoords(){
 
 function hideError()
 {
+    document.getElementById("eContainer").style.display = "none";
+}
 
+function hideResult()
+{
+    document.getElementById("rContainer").style.display="none";
 }
