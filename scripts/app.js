902d741e675320893616bef26db314d6d33f8ddd
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-02-21 14:14:13 -0600

    Implementando funcion para validar y obtener los nuevos resultados

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-
+    getNewCoords();
 }
 
 function setInputFilter(textbox, inputFilter) {
@@ -26,3 +26,24 @@ setInputFilter(document.getElementById("cord-x"), function (value) {
 setInputFilter(document.getElementById("cord-z"), function (value) {
     return /^-?\d*[.,]?\d*$/.test(value);
 });
+
+const getNewCoords = () => {
+    let form = document.forms['form'];
+    let optionValue;
+    let coord_x;
+    let coord_z;
+    form['btn-cord'].addEventListener("click", () => {
+        optionValue = form['operation-value'].value;
+        coord_x = form['cord-x'].value;
+        coord_z = form['cord-z'].value;
+        if (coord_x != "" && coord_z != "") {
+            if (optionValue == "ow") {
+                
+            } else if (optionValue == "ne") {
+        
+            }
+        } else {
+            
+        }
+    });
+}
