9410ebc5732f6aec218ce2545435c2deacdf54a7
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-02-21 20:06:50 -0600

    Clase terminada

--- scripts/success.js
+++ scripts/success.js
@@ -1,7 +1,21 @@
-class Success {
+class Coordinate {
     static coordCounter = 0;
-    constructor() {
-        this._id = ++Success.coordCounter;
-        
+    constructor(coord_x, coord_z) {
+        this._id = ++Coordinate.coordCounter;
+        this._x = coord_x;
+        this._z = coord_z;
     }
+
+    get id() {
+        return this._id;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    get z() {
+        return this._z;
+    }
+
 }
