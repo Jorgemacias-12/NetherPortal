63d44f1b3611f88aca10b5f06c4bc256a7e13e32
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-03-28 11:43:45 -0600

    Limpiando código en este archivo, de momento se ha cambiado la función de validación a su propio archivo.

--- scripts/app.js
+++ scripts/app.js
@@ -4,32 +4,6 @@ const results = [];
 window.onload = () => {
     getNewCoords();
 }
-
-function setInputFilter(textbox, inputFilter) {
-    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
-        textbox.addEventListener(event, function () {
-            if (inputFilter(this.value)) {
-                this.oldValue = this.value;
-                this.oldSelectionStart = this.selectionStart;
-                this.oldSelectionEnd = this.selectionEnd;
-            } else if (this.hasOwnProperty("oldValue")) {
-                this.value = this.oldValue;
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-            } else {
-                this.value = "";
-            }
-        });
-    });
-}
-
-setInputFilter(document.getElementById("cord-x"), function (value) {
-    return /^-?\d*[.,]?\d*$/.test(value);
-});
-
-setInputFilter(document.getElementById("cord-z"), function (value) {
-    return /^-?\d*[.,]?\d*$/.test(value);
-});
-
 const getNewCoords = () => {
     let form = document.forms['form'];
     let optionValue;
